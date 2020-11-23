965caab0856e3dae0426816641a40a13895c2e65
Orlando <lopezorlando23@gmail.com>
2020-11-23 08:51:32 -0700

    Additional Shopping Cart Models
    Added the Shopping Cart class as well as the Shopping Cart Items Class and did a database migration

--- BethanysPieShop/BethanysPieShop/Models/AppDbContext.cs
+++ BethanysPieShop/BethanysPieShop/Models/AppDbContext.cs
@@ -17,6 +17,10 @@ namespace BethanysPieShop.Models
 
         public DbSet<Pie> Pies { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+
+
 
 
 
