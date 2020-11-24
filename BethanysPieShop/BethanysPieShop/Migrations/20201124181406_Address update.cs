c56083ad4b7e35a94c6b80f569ae5ab96cfd0f81
Orlando <lopezorlando23@gmail.com>
2020-11-24 11:30:02 -0700

    Order Controller Added

---
+++ BethanysPieShop/BethanysPieShop/Migrations/20201124181406_Address update.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BethanysPieShop.Migrations
+{
+    public partial class Addressupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AdressLine1",
+                table: "Orders",
+                newName: "AddressLine1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AddressLine1",
+                table: "Orders",
+                newName: "AdressLine1");
+        }
+    }
+}
--- BethanysPieShop/BethanysPieShop/Models/Order.cs
+++ BethanysPieShop/BethanysPieShop/Models/Order.cs
@@ -11,7 +11,7 @@ namespace BethanysPieShop.Models
         public List<OrderDetail> OrderDetails { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string AdressLine1 { get; set; }
+        public string AddressLine1 { get; set; }
         public string AddressLine2 { get; set; }
         public string ZipCode { get; set; }
         public string City { get; set; }
